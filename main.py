0c0173d4b05945a3eaa77c5315b1c876b2bbd71d
anton-logom <anton-logom@yandex.ru>
2020-09-15 19:59:51 +0300

    add selection sort

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ class ToolBase:
 
 class bubble_sort(ToolBase):
     """сортировка пузырьком"""
-
     def sort(nums):
         # We set swapped to True so the loop looks runs at least once
         swapped = True
@@ -38,7 +37,6 @@ class bubble_sort(ToolBase):
 
 class insertion_sort(ToolBase):
     """сортировка вставками"""
-
     def sort(nums):
         # Начнем со второго элемента, так как мы предполагаем, что первый элемент отсортирован
         for i in range(1, len(nums)):
@@ -52,7 +50,19 @@ class insertion_sort(ToolBase):
             # Вставляем элемент
             nums[j + 1] = item_to_insert
 
-
+class selection_sort(ToolBase):
+    """сортирвка выбором"""
+    def selection_sort(nums):
+        # значение i соответствует тому, сколько значений было отсортировано
+        for i in range(len(nums)):
+            # Мы предполагаем, что первый элемент несортированного сегмента является наименьшим
+            lowest_value_index = i
+            # Этот цикл перебирает несортированные элементы
+            for j in range(i + 1, len(nums)):
+                if nums[j] < nums[lowest_value_index]:
+                    lowest_value_index = j
+            # Поменять местами значения самого низкого несортированного элемента с первым несортированным
+            nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
 
 
 class BubbleArray(SortArray):
@@ -64,10 +74,14 @@ class InsertionArray(SortArray):
     """массив с сортирвокй вставками"""
     tool = insertion_sort()
 
+class SelectionArray(SortArray):
+    """массив с сортирвокй выбором"""
+    tool = selection_sort()
+
 
 array1 = BubbleArray([5, 2, 1, 8, 4])
 
-array2 = InsertionArray([9, 1, 15, 28, 6])
+array2 = SelectionArray([9, 1, 15, 28, 6])
 
 array3 = InsertionArray([9, 1, 15, 28, 6])
 
@@ -76,7 +90,7 @@ print ('выполняем для массива 1 сортировку пузы
 print(array1.nums)
 
 array2.sort()
-print('выполняем для массива 2 сортировку вставками')
+print('выполняем для массива 2 сортировку выбором')
 print(array2.nums)
 
 array3.setTool(bubble_sort())
